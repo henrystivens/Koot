df3199195d49da018dad64acc45c4eb82fc08505
Henry Stivens Adarme <henrystivens@users.noreply.github.com>
2025-02-15 11:41:48 -0500

    Revert "Create scaffold"

--- koot/libs/controller_scaffold_lite.php
+++ koot/libs/controller_scaffold_lite.php
@@ -8,9 +8,9 @@ abstract class ControllerScaffoldLite extends ControllerAdmin
 {
 
     /** Folder in views/_shared/scaffolds/ */
-    public $scaffold = 'lite';
+    public string $scaffold = 'lite';
     /** Model Name in CamelCase */
-    public $model = '';
+    public string $model = '';
     /** Number of records per page */
     public int $perPage = 30;
 
@@ -33,48 +33,42 @@ abstract class ControllerScaffoldLite extends ControllerAdmin
     public function create()
     {
         // It is verified if the data has been sent via POST
-        if (Input::hasPost($this->model)) {
+        if (Input::hasPost('data')) {
             $obj = new $this->model;
             // Try to save the user
-            if (!$obj->create(Input::post($this->model))) {
-                // If it fails the data is persistent in the form
-                $this->{$this->model} = $obj;
+            if ($obj->create(Input::post('data'))) {
+                // Success message and return to the list
+                Flash::valid(_('Record created '));
+                Redirect::toAction('');
                 return;
             }
-
-            // Success message and return to the list
-            Flash::valid(_('Record created'));
-            Redirect::toAction('');
-            return;
+            // If it fails the data is persistent in the form
+            $this->data = Input::post('data');
         }
-
-        $this->{$this->model} = (new $this->model);
     }
 
     public function edit(int $id)
     {
+        View::select('create');
         // Load the data from database
-        $this->{$this->model} = $this->model::get($id);
+        $this->data = $this->model::get($id);
         //If not exist
-        if (!$this->{$this->model}) {
+        if (!$this->data) {
             Flash::warning(_('Record not found'));
             Redirect::toAction('');
             return;
         }
-
-        View::select('create');
-
         // It is verified if the data has been sent via POST
-        if (Input::hasPost($this->model)) {
+        if (Input::hasPost('data')) {
             // Try to save changes
-            if (!$this->{$this->model}->update(Input::post($this->model))) {
-                Flash::error(_('Something was wrong'));
+            if ($this->data->update(Input::post('data'))) {
+                // Success message and return to the list
+                Flash::valid(_('Record updated'));
+                Redirect::toAction('');
                 return;
             }
-
-            // Success message and return to the list
-            Flash::valid(_('Record updated'));
-            Redirect::toAction('');
+            // If it fails the data is persistent in the form
+            $this->data = Input::post('data');
         }
     }
 
